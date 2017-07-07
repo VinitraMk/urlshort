5913b36b0abc73905864c077f2261d0962938c3e
VinitraMk <vinitramk@gmail.com>
2017-07-08 00:00:36 +0530

    Changed the base code to include forms

--- shortener/admin.py
+++ shortener/admin.py
@@ -4,11 +4,8 @@
 from django.contrib import admin
 from shortener.models import Urls
 
-class UrlsAdmin(admin.ModelAdmin):
-    listDisplay=('shortId','httpUrl','timeStamp')
-    order=('-timeStamp',)
 
-admin.site.register(Urls,UrlsAdmin)
+admin.site.register(Urls)
 
 
 # Register your models here.
